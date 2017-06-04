851ea505fcdf48a79bbf750e6fbf03e1e6628527
carrjona <carrjona@ecs.vuw.ac.nz>
2017-06-04 18:49:16 +1200

    Added KpsModel.println() method

--- Code/src/model/KpsModel.java
+++ Code/src/model/KpsModel.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Calendar;
+
 /**
  * Created by Jack on 6/3/2017.
  */
@@ -7,12 +9,12 @@ package model;
 public class KpsModel {
 
 	private RouteMap routeMap;
-	
+
 	public KpsModel(){
 		routeMap = new RouteMap();
 		addTestMapData();
 	}
-	
+
 	private void addTestMapData(){
 		//Dummy Locations
 		routeMap.addLocation("Hastings", 0.00, 0.00);
@@ -20,7 +22,13 @@ public class KpsModel {
 		routeMap.addLocation("Porirua", 0.00, 0.00);
 		routeMap.addLocation("Karori", 0.00, 0.00, 45);
 	}
-	
+
+	public static void println(String s){
+		Calendar cal = Calendar.getInstance();
+		System.out.printf("[%02d/%02d/%04d %02d:%02d:%02d] %s", cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH),
+				cal.get(Calendar.YEAR), cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND), s);
+	}
+
 	public static void main(String[] args){
 		new KpsModel();
 	}
--- Code/src/model/RouteMap.java
+++ Code/src/model/RouteMap.java
@@ -18,11 +18,11 @@ public class RouteMap {
 	
 	public boolean addLocation(String name, double latitude, double longitude, int id){
 		if(locations.containsKey(id)){
-			System.out.printf("Failed to add location, id %d already in use.\n", id);
+			KpsModel.println("Failed to add location, id " + id + " already in use.");
 			return false;
 		}else{
 			locations.put(id, new Location(name, latitude, longitude, id));
-			System.out.printf("Added location %s (%.2f, %.2f) with id: %d\n", name, latitude, longitude, id);
+			KpsModel.println(String.format("Added location %s (%.2f, %.2f) with id: %d\n", name, latitude, longitude, id));
 			return true;
 		}
 	}
@@ -38,6 +38,7 @@ public class RouteMap {
 	}
 	
 	public int addSegment(int id, int originId, int destinationId){
+		
 		segments.put(id, new Segment(id, locations.get(originId), locations.get(destinationId)));
 		System.out.printf("Added segment from %s to %s with id: %d\n", locations.get(originId).getName(), locations.get(destinationId).getName(), id);
 		return id;
