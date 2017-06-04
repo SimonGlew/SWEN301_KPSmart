5955d3d309a324c7f359d6d17cbbd04eafdb3155
Jack Slater <slaterjack@citron.ecs.vuw.ac.nz>
2017-06-04 21:38:10 +1200

    added parser read for transport cost update

--- Code/src/storage/KpsDatabase.java
+++ Code/src/storage/KpsDatabase.java
@@ -6,6 +6,11 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import model.KpsModel;
 
 /**
  * Created by Jack on 6/3/2017.
@@ -14,13 +19,14 @@ public class KpsDatabase {
 
 	static final String LOGFILE = "logfile.xml";
 
-	ArrayList<BusinessEvent> BusinessEvents;
+	ArrayList<BusinessEvent> businessEvents;
 
 	public KpsDatabase(){
 		loadEvents();
 	}
 
 	public void loadEvents(){
+		businessEvents = new ArrayList<>();
 		Document dom;
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
@@ -28,13 +34,103 @@ public class KpsDatabase {
 
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			dom = db.parse(LOGFILE);
+			Element e = dom.getDocumentElement();
+			NodeList nodes = e.getChildNodes();
 
 
+			for(int i = 0; i< nodes.getLength(); i++){
+				Node node = nodes.item(i);
+
+				switch(node.getNodeName()){
+					case "TransportCostUpdate":
+						businessEvents.add(parseTransportCostUpdate(node));
+						break;
+
+					default:
+						break;
+				}
+			}
 		}
 		catch(Exception e){
 
 		}
 
+	}
+
+	public TransportCostUpdate parseTransportCostUpdate(Node node){
+		TransportCostUpdate transportCostUpdate = new TransportCostUpdate();
+		NodeList childNodes = node.getChildNodes();
+
+		for(int j =  0; j < childNodes.getLength(); j++){
+			Node data = childNodes.item(j);
+			switch(data.getNodeName()){
+				case "Company":
+					transportCostUpdate.setCompany(data.getTextContent());
+					break;
+				case "To":
+					transportCostUpdate.setTo(data.getTextContent());
+					break;
+				case "From":
+					transportCostUpdate.setFrom(data.getTextContent());
+					break;
+				case "Priority":
+					transportCostUpdate.setPriority(Integer.parseInt(data.getTextContent()));
+					break;
+				case "WeightCost":
+					transportCostUpdate.setWeightCost(Double.parseDouble(data.getTextContent()));
+					break;
+				case "VolumeCost":
+					transportCostUpdate.setVolumeCost(Double.parseDouble(data.getTextContent()));
+					break;
+				case "MaxWeight":
+					transportCostUpdate.setMaxWeight(Double.parseDouble(data.getTextContent()));
+					break;
+				case "MaxVolume":
+					transportCostUpdate.setMaxVolume(Double.parseDouble(data.getTextContent()));
+					break;
+				case "Duration":
+					transportCostUpdate.setDuration(Double.parseDouble(data.getTextContent()));
+					break;
+				case "Frequency":
+					transportCostUpdate.setFrequency(Integer.parseInt(data.getTextContent()));
+					break;
+				case "Days":
+					ArrayList<KpsModel.Day> days = new ArrayList<>();
+					NodeList dayNodes = data.getChildNodes();
+					for(int d = 0; d< dayNodes.getLength(); d++){
+						Node dayNode = dayNodes.item(d);
+						switch(dayNode.getTextContent()){
+							case "Monday":
+								days.add(KpsModel.Day.Monday);
+								break;
+							case "Tuesday":
+								days.add(KpsModel.Day.Tuesday);
+								break;
+							case "Wednesday":
+								days.add(KpsModel.Day.Wednesday);
+								break;
+							case "Thursday":
+								days.add(KpsModel.Day.Thursday);
+								break;
+							case "Friday":
+								days.add(KpsModel.Day.Friday);
+								break;
+							case "Saturday":
+								days.add(KpsModel.Day.Saturday);
+								break;
+							case "Sunday":
+								days.add(KpsModel.Day.Sunday);
+								break;
+						}
+					}
+					transportCostUpdate.setDays(days);
+					break;
+
+			}
+
+		}
+
+		return transportCostUpdate;
 	}
 
 
--- Code/src/storage/TransportCostUpdate.java
+++ Code/src/storage/TransportCostUpdate.java
@@ -17,21 +17,107 @@ public class TransportCostUpdate  implements BusinessEvent{
 	int frequency;
 	List<KpsModel.Day> days;
 
-	public TransportCostUpdate(String company, String to, String from, int priority, double weightCost,
-			double volumeCost, double maxWeight, double maxVolume, double duration, int frequency, List<KpsModel.Day> days) {
-		super();
+	public TransportCostUpdate() {
+
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
 		this.company = company;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
 		this.to = to;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
 		this.from = from;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
 		this.priority = priority;
+	}
+
+	public double getWeightCost() {
+		return weightCost;
+	}
+
+	public void setWeightCost(double weightCost) {
 		this.weightCost = weightCost;
+	}
+
+	public double getVolumeCost() {
+		return volumeCost;
+	}
+
+	public void setVolumeCost(double volumeCost) {
 		this.volumeCost = volumeCost;
+	}
+
+	public double getMaxWeight() {
+		return maxWeight;
+	}
+
+	public void setMaxWeight(double maxWeight) {
 		this.maxWeight = maxWeight;
+	}
+
+	public double getMaxVolume() {
+		return maxVolume;
+	}
+
+	public void setMaxVolume(double maxVolume) {
 		this.maxVolume = maxVolume;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(double duration) {
 		this.duration = duration;
+	}
+
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(int frequency) {
 		this.frequency = frequency;
+	}
+
+	public List<KpsModel.Day> getDays() {
+		return days;
+	}
+
+	public void setDays(List<KpsModel.Day> days) {
 		this.days = days;
 	}
+
+	@Override
+	public String toString() {
+		return "TransportCostUpdate [company=" + company + ", to=" + to + ", from=" + from + ", priority=" + priority
+				+ ", weightCost=" + weightCost + ", volumeCost=" + volumeCost + ", maxWeight=" + maxWeight
+				+ ", maxVolume=" + maxVolume + ", duration=" + duration + ", frequency=" + frequency + ", days=" + days
+				+ "]";
+	}
+
+	
 
 
 }
