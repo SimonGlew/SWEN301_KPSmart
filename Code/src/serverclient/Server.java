dcaef405fd3359d0dd96608fd39f91ef134343c0
glewsimo <glewsimo@ecs.vuw.ac.nz>
2017-06-04 20:28:20 +1200

    removed unused import on server.java

--- Code/src/serverclient/Server.java
+++ Code/src/serverclient/Server.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import javax.security.auth.kerberos.KerberosPrincipal;
-
 import io.ServerParser;
 import model.KpsModel;
 
