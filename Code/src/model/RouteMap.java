2ccb2c6f39d099ab458d9f04bb518dffeb36386c
carrjona <carrjona@ecs.vuw.ac.nz>
2017-06-04 18:52:22 +1200

    Utilising new println mehtod

--- Code/src/model/RouteMap.java
+++ Code/src/model/RouteMap.java
@@ -7,15 +7,15 @@ import java.util.Map;
  * Created by Jack on 6/3/2017.
  */
 public class RouteMap {
-	
+
 	private Map<Integer, Location> locations;
 	private Map<Integer, Segment> segments;
-	
+
 	public RouteMap(){
 		locations = new HashMap<Integer, Location>();
 		segments = new HashMap<Integer, Segment>();
 	}
-	
+
 	public boolean addLocation(String name, double latitude, double longitude, int id){
 		if(locations.containsKey(id)){
 			KpsModel.println("Failed to add location, id " + id + " already in use.");
@@ -26,21 +26,21 @@ public class RouteMap {
 			return true;
 		}
 	}
-	
+
 	public int addLocation(String name, double latitude, double longitude){
 		int id = 1;
 		while(locations.containsKey(id)) {
 			id++;
 		}
 		locations.put(id,  new Location(name, latitude, longitude, id));
-		System.out.printf("Added location %s (%.2f, %.2f) with id: %d\n", name, latitude, longitude, id);
+		KpsModel.println(String.format("Added location %s (%.2f, %.2f) with id: %d\n", name, latitude, longitude, id));
 		return id;
 	}
-	
+
 	public int addSegment(int id, int originId, int destinationId){
-		
+
 		segments.put(id, new Segment(id, locations.get(originId), locations.get(destinationId)));
-		System.out.printf("Added segment from %s to %s with id: %d\n", locations.get(originId).getName(), locations.get(destinationId).getName(), id);
+		KpsModel.println(String.format("Added segment from %s to %s with id: %d\n", locations.get(originId).getName(), locations.get(destinationId).getName(), id));
 		return id;
 	}
 }
