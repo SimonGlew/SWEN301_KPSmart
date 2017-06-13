b07e936e30c97cbba3c8c74c5e86825ce5d689f6
Dallan Freemantle <freemadall@ecs.vuw.ac.nz>
2017-06-14 02:42:24 +1200

    Updated AmountOfMailPanel

--- Code/src/gui/AmountOfMailPanel.java
+++ Code/src/gui/AmountOfMailPanel.java
@@ -27,6 +27,11 @@ public class AmountOfMailPanel extends JPanel {
 	private void initPanel() {
 		setBackground(UIManager.getColor("Panel.background"));
 		setLayout(null);
+		
+				JLabel titleLabel = new JLabel("Mail Statistics");
+				titleLabel.setFont(new Font("SansSerif", Font.BOLD, 31));
+				titleLabel.setBounds(100, 11, 209, 31);
+				add(titleLabel);
 
 		JLabel originLabel = new JLabel("Origin");
 		originLabel.setForeground(Color.DARK_GRAY);
@@ -54,50 +59,40 @@ public class AmountOfMailPanel extends JPanel {
 		destComboBox.setBounds(150, 116, 170, 26);
 		add(destComboBox);
 
-		JLabel noItemsLabel = new JLabel("Number of Items");
+		JLabel noItemsTitleLabel = new JLabel("Number of Items");
+		noItemsTitleLabel.setForeground(Color.DARK_GRAY);
+		noItemsTitleLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		noItemsTitleLabel.setBounds(30, 164, 121, 26);
+		add(noItemsTitleLabel);
+		
+		JLabel noItemsLabel = new JLabel("0");
 		noItemsLabel.setForeground(Color.DARK_GRAY);
 		noItemsLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		noItemsLabel.setBounds(30, 164, 121, 26);
+		noItemsLabel.setBounds(163, 164, 82, 26);
 		add(noItemsLabel);
 
-		JLabel volumeLabel = new JLabel("Total Volume");
+		JLabel volumeTitleLabel = new JLabel("Total Volume");
+		volumeTitleLabel.setForeground(Color.DARK_GRAY);
+		volumeTitleLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		volumeTitleLabel.setBounds(30, 211, 93, 26);
+		add(volumeTitleLabel);
+		
+		JLabel volumeLabel = new JLabel("0.00");
 		volumeLabel.setForeground(Color.DARK_GRAY);
 		volumeLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		volumeLabel.setBounds(30, 211, 93, 26);
+		volumeLabel.setBounds(163, 211, 82, 26);
 		add(volumeLabel);
 
-		JLabel weightLabel = new JLabel("Total Weight");
+		JLabel weightTitleLabel = new JLabel("Total Weight");
+		weightTitleLabel.setForeground(Color.DARK_GRAY);
+		weightTitleLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		weightTitleLabel.setBounds(30, 255, 121, 26);
+		add(weightTitleLabel);
+		
+		JLabel weightLabel = new JLabel("0.00");
 		weightLabel.setForeground(Color.DARK_GRAY);
 		weightLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		weightLabel.setBounds(30, 255, 121, 26);
+		weightLabel.setBounds(163, 255, 82, 26);
 		add(weightLabel);
-
-		JButton submitButton = new JButton("Submit");
-		submitButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
-		submitButton.setBounds(150, 300, 108, 31);
-		add(submitButton);
-
-		JLabel titleLabel = new JLabel("Mail Statistics");
-		titleLabel.setFont(new Font("SansSerif", Font.BOLD, 31));
-		titleLabel.setBounds(100, 11, 209, 31);
-		add(titleLabel);
-		
-		JLabel label = new JLabel("0");
-		label.setForeground(Color.DARK_GRAY);
-		label.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		label.setBounds(163, 164, 82, 26);
-		add(label);
-		
-		JLabel label_1 = new JLabel("0.00");
-		label_1.setForeground(Color.DARK_GRAY);
-		label_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		label_1.setBounds(163, 211, 82, 26);
-		add(label_1);
-		
-		JLabel label_2 = new JLabel("0.00");
-		label_2.setForeground(Color.DARK_GRAY);
-		label_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		label_2.setBounds(163, 255, 82, 26);
-		add(label_2);
 	}
 }
