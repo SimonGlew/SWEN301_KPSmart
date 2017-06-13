80e9f66c8f9678ab6747ade17d2ee657a6e3b462
Dallan Freemantle <freemadall@ecs.vuw.ac.nz>
2017-06-14 02:54:47 +1200

    Add CriticalRoutesPanel

---
+++ Code/src/gui/CriticalRoutesPanel.java
@@ -0,0 +1,66 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import javax.swing.UIManager;
+import serverclient.ClientController;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+@SuppressWarnings("serial")
+public class CriticalRoutesPanel extends JPanel {
+	private JTable table;
+
+	public CriticalRoutesPanel() {
+		initPanel();
+	}
+
+	private void initPanel() {
+		setBackground(UIManager.getColor("Panel.background"));
+		setLayout(null);
+		
+				JLabel titleLabel = new JLabel("Critical Routes");
+				titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				titleLabel.setVerticalAlignment(SwingConstants.TOP);
+				titleLabel.setFont(new Font("SansSerif", Font.BOLD, 31));
+				titleLabel.setBounds(10, 11, 430, 47);
+				add(titleLabel);
+				
+				table = new JTable();
+				table.setModel(new DefaultTableModel(
+					new Object[][] {
+						{"Origin", "Destination", "Average Difference (Per Item)"},
+						{null, null, null},
+					},
+					new String[] {
+						"Origin", "Destination", "Average Difference (Per Item)"
+					}
+				) {
+					Class[] columnTypes = new Class[] {
+						String.class, String.class, String.class
+					};
+					public Class getColumnClass(int columnIndex) {
+						return columnTypes[columnIndex];
+					}
+					boolean[] columnEditables = new boolean[] {
+						false, false, false
+					};
+					public boolean isCellEditable(int row, int column) {
+						return columnEditables[column];
+					}
+				});
+				table.getColumnModel().getColumn(2).setPreferredWidth(154);
+				table.setBounds(10, 73, 430, 149);
+				add(table);
+	}
+}
