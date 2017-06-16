e63e8be7fb7bfbf489fa367d6ccc5d22eb7c730f
bondkyal <bondkyal@ecs.vuw.ac.nz>
2017-06-16 14:37:01 +1200

    fixed login close

--- Code/src/gui/LoginDialog.java
+++ Code/src/gui/LoginDialog.java
@@ -7,9 +7,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -32,32 +34,39 @@ public class LoginDialog implements ActionListener{
 		loginBox.setResizable(false);
 		loginBox.setUndecorated(true);
 		loginBox.setSize(new Dimension(420,130));
-		loginBox.setPreferredSize(new Dimension(420,110));
+		loginBox.setPreferredSize(new Dimension(420,130));
 		loginBox.setLocation((gui.frame.getWidth()/2)-(loginBox.getWidth()/2), (gui.frame.getHeight()/2)-(loginBox.getHeight()/2));
 		loginBox.getContentPane().setLayout(new FlowLayout());
 		loginBox.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		
+		JPanel border = new JPanel();
+		border.setPreferredSize(new Dimension(400,118));
+		border.setBounds(0, 0, 400, 118);
+		border.setBorder(BorderFactory.createEtchedBorder());
 
 		JLabel usernameLabel = new JLabel("Username:");
 		usernameLabel.setPreferredSize(new Dimension(100, 30));
 		usernameField = new JTextField();
-		usernameField.setPreferredSize(new Dimension(300, 30));
-		loginBox.getContentPane().add(usernameLabel);
-		loginBox.getContentPane().add(usernameField);
+		usernameField.setPreferredSize(new Dimension(270, 30));
+		border.add(usernameLabel);
+		border.add(usernameField);
 
 		JLabel password = new JLabel("Password");
 		password.setPreferredSize(new Dimension(100, 30));
 		passwordField = new JPasswordField();
-		passwordField.setPreferredSize(new Dimension(300, 30));
-		loginBox.getContentPane().add(password);
-		loginBox.getContentPane().add(passwordField);
+		passwordField.setPreferredSize(new Dimension(270, 30));
+		border.add(password);
+		border.add(passwordField);
 
 		submit = new JButton("Submit");
-		loginBox.getContentPane().add(submit);
+		border.add(submit);
 		submit.addActionListener(this);
 
 		close = new JButton("Close");
-		loginBox.getContentPane().add(close);
+		border.add(close);
 		close.addActionListener(this);
+		
+		loginBox.add(border);
 
 		loginBox.pack();
 		loginBox.setVisible(true);
