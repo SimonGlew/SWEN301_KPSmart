91aa4e41f1a535d7685adba9b3274fe59aa3046b
Dallan Freemantle <freemadall@ecs.vuw.ac.nz>
2017-06-16 15:14:42 +1200

    Updated Event Nav layout

--- Code/src/gui/EventNavigationPanel.java
+++ Code/src/gui/EventNavigationPanel.java
@@ -32,98 +32,72 @@ public class EventNavigationPanel extends JPanel {
 		setBackground(UIManager.getColor("Panel.background"));
 
 		JLabel titleLabel = new JLabel("Event Navigation");
+		titleLabel.setBounds(10, 11, 430, 40);
 		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setFont(new Font("SansSerif", Font.BOLD, 31));
 		
 		JButton btnNext = new JButton("Next");
+		btnNext.setBounds(330, 425, 110, 40);
 		
 		JButton button = new JButton("Previous");
+		button.setBounds(10, 425, 110, 40);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
 		
 		JLabel eventDetailsLabel = new JLabel("Event Details");
+		eventDetailsLabel.setBounds(20, 70, 134, 14);
 		eventDetailsLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
 		eventDetailsPanel = new JPanel();
+		eventDetailsPanel.setBounds(20, 90, 408, 188);
 		
 		JLabel businessFiguresLabel = new JLabel("Business Figures");
+		businessFiguresLabel.setBounds(20, 289, 134, 20);
 		businessFiguresLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
 		JPanel busFigsPanel = new JPanel();
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(10)
-					.addComponent(titleLabel, GroupLayout.DEFAULT_SIZE, 430, Short.MAX_VALUE)
-					.addGap(10))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(button, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 210, Short.MAX_VALUE)
-					.addComponent(btnNext, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(20)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(eventDetailsLabel, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
-							.addGap(22))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(eventDetailsPanel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(busFigsPanel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 408, Short.MAX_VALUE)
-								.addComponent(businessFiguresLabel, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE))
-							.addContainerGap())))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(11)
-					.addComponent(titleLabel)
-					.addGap(19)
-					.addComponent(eventDetailsLabel, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(eventDetailsPanel, GroupLayout.DEFAULT_SIZE, 188, Short.MAX_VALUE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(businessFiguresLabel)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(busFigsPanel, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(button, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNext, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
-		);
-		busFigsPanel.setLayout(null);
+		busFigsPanel.setBounds(20, 315, 408, 99);
+		
+		JLabel eventNumberLabel = new JLabel("0/0");
+		eventNumberLabel.setBounds(207, 438, 40, 14);
+		eventNumberLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JLabel label = new JLabel("Total Revenue");
+		label.setBounds(111, 11, 108, 26);
 		label.setForeground(Color.DARK_GRAY);
 		label.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		label.setBounds(111, 11, 108, 26);
-		busFigsPanel.add(label);
 		
 		JLabel label_1 = new JLabel("Total Expenditure");
+		label_1.setBounds(111, 58, 121, 26);
 		label_1.setForeground(Color.DARK_GRAY);
 		label_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		label_1.setBounds(111, 58, 121, 26);
-		busFigsPanel.add(label_1);
 		
 		JLabel label_2 = new JLabel("$0.00");
+		label_2.setBounds(243, 58, 45, 26);
 		label_2.setForeground(Color.BLACK);
 		label_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		label_2.setBounds(243, 58, 45, 26);
-		busFigsPanel.add(label_2);
 		
 		JLabel label_3 = new JLabel("$0.00");
+		label_3.setBounds(243, 11, 45, 26);
 		label_3.setForeground(Color.BLACK);
 		label_3.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		label_3.setBounds(243, 11, 45, 26);
+		busFigsPanel.setLayout(null);
+		busFigsPanel.add(label);
+		busFigsPanel.add(label_1);
+		busFigsPanel.add(label_2);
 		busFigsPanel.add(label_3);
 		eventDetailsPanel.setLayout(new CardLayout(0, 0));
-		setLayout(groupLayout);
+		setLayout(null);
+		add(eventNumberLabel);
+		add(titleLabel);
+		add(button);
+		add(btnNext);
+		add(busFigsPanel);
+		add(businessFiguresLabel);
+		add(eventDetailsPanel);
+		add(eventDetailsLabel);
 	}
 	
 	/**
